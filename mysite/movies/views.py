57c0c54407ebabd9382a93076c28a5d8f90a7722
jbellogo <jbnermal3@gmail.com>
2020-06-14 02:04:46 -0400

    backend first

--- mysite/movies/views.py
+++ mysite/movies/views.py
@@ -24,4 +24,18 @@ def new_search(request):
     return render(request, 'new_search.html', things_for_frontend)
 
 def filtered_search(request):
-    return render(request, 'filtered_search.html')
+    rank_by = request.POST.get('rank_by')
+    genre = request.POST.get('genre')
+    year = request.POST.get('year')
+    month = request.POST.get('month')
+    day = request.POST.get('day')
+    things_for_pandas = {
+        'rank_by': rank_by,
+        'genre' : genre,
+        'year' : year,
+        'month' : month,
+        'day' : day,
+    }
+    print(things_for_frontend)
+    list_for_frontend = []
+    return render(request, 'filtered_search.html', list_for_frontend)
