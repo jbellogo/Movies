014c75bc230244794d92ba78c0fc9105f2ca6a1b
jbellogo <jbnermal3@gmail.com>
2020-06-27 00:49:44 -0400

    ML engine functionality

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -7,9 +7,9 @@ class Search(models.Model):
     search= models.CharField(max_length=200)
     date = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return '{}'.format(self.search)
 
     class Meta:
         verbose_name_plural= "Searches"  # specifies the plural
+
